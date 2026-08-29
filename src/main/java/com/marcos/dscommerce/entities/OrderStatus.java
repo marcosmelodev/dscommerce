57b3b7db8fb0573f825fb9c6b8e505818a37b2a8
Marcos Melo <marcos.dhl@hotmail.com>
2026-08-29 18:44:30 -0300

    User, OrderStatus and Order entities

---
+++ src/main/java/com/marcos/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.marcos.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
---
+++ src/test/java/com/marcos/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
