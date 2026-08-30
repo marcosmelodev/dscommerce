4c255818c42421851d1f0dc7f5fe800dd0075906
Marcos Melo <marcos.dhl@hotmail.com>
2026-08-30 19:59:25 -0300

    OrderItem, OrderItemPk

---
+++ src/main/java/com/marcos/dscommerce/entities/OrderItemPk.java
@@ -0,0 +1,39 @@
+package com.marcos.dscommerce.entities;
+
+
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+// auxiliary class to represent the composite primary key
+
+@Embeddable
+public class OrderItemPk {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItemPk(){}
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
