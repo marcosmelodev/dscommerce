0fd0ee9d0cef9616abbbde84db73ca72cc5b601e
Marcos Melo <marcos.dhl@hotmail.com>
2026-09-14 18:35:54 -0300

    GET product by id

---
+++ src/main/java/com/marcos/dscommerce/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.marcos.dscommerce.controllers;
+
+import com.marcos.dscommerce.dto.ProductDTO;
+import com.marcos.dscommerce.entities.Product;
+import com.marcos.dscommerce.repositories.ProductRepository;
+import com.marcos.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+}
