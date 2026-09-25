e6b3644b666b904e5102a5ae352ea50bcac8ac14
Marcos Melo <marcos.dhl@hotmail.com>
2026-09-25 19:49:28 -0300

    Bean Validation

--- src/main/java/com/marcos/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/marcos/dscommerce/dto/ProductDTO.java
@@ -2,12 +2,20 @@ package com.marcos.dscommerce.dto;
 
 import com.marcos.dscommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10,message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
